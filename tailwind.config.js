18e38d1e5406a0033dc3432b29cb739045b62102
Jayanth27092002 <128683978+Jayanth27092002@users.noreply.github.com>
2023-06-24 23:05:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"]
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightblue:"#2b84ea",
+        greenLight:"#61cea6",
+        lightblue500:"#0b72e7",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        grayText: "#818597",
+        gray2: "#525a76",
+        
+
+      }
+    },
+  },
+  plugins: [],
+}
+
